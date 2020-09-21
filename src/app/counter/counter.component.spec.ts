21865b90ff63dc233bf4adb1f5313c54044bd7ac
Sammy Abdulkader <46463233+sabdul2021@users.noreply.github.com>
2020-09-21 09:58:15 -0400

    Adding changes and console logging output values

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -36,7 +36,9 @@ describe('CounterComponent', () => {
 
         //assert
         expect(newValue).toBeGreaterThan(initialValue);
-        console.log(newValue, initialValue);
+        //newValue prints out 3 
+        //initialValue prints out 2
+        console.log(newValue + ' is greater than ' + initialValue);
     });
 
     it('should decrement the counter number by one', () => {
@@ -44,38 +46,41 @@ describe('CounterComponent', () => {
         var initialValue = component.counter;
 
         //act
-        component.increment();
+        component.decrement();
         fixture.detectChanges();
         var newValue = component.counter;
 
         //assert
-      expect(initialValue).toBeLessThan(newValue);
-      console.log(newValue, initialValue);
+        expect(newValue).toBeLessThan(initialValue);
+        //newValue prints out 1
+        //initialValue prints out 2
+        console.log(newValue + ' is less than ' + initialValue);
     });
 
     it('should display the current number of the counter', () => {
-       //assert the number of your screen is of number: 1
+       //assert the number of your screen is of number: 2
         fixture.detectChanges();
         expect(htmlElement.textContent).toEqual('Number: 2');
-        console.log(htmlElement.textContent);
+        console.log('Show print out two: ' + htmlElement.textContent);
     });
 
-    it('should display the counter number on screen, after being incremented by one', () => {
+    it('should display the counter number on screen, after being incremented to three', () => {
         component.increment();
         fixture.detectChanges();
-        //assert the number of your screen is of number: 1
+        //assert the number of your screen is of number: 3
         expect(htmlElement.textContent).toEqual('Number: 3');
-        console.log(htmlElement.textContent);
+        console.log('The value is incremented to three: ' + htmlElement.textContent);
     });
 
-    it('should display the counter number on screen, after being decremented by one', () => {
+    it('should display the counter number on screen, after being decremented to one', () => {
         component.decrement();
         fixture.detectChanges();
         //assert the number of your screen is of number: 1
         expect(htmlElement.textContent).toEqual('Number: 1');
+        console.log('The value is decremented by 1: ' + htmlElement.textContent);
     });
 
-    it ('adding another number', () => {
+    it ('adding another incremented number', () => {
         component.increment();
         fixture.detectChanges();
         fixture.autoDetectChanges();
@@ -83,6 +88,6 @@ describe('CounterComponent', () => {
         //let me asssert some changes to the numbers increasing
         expect(htmlElement.textContent).toEqual('Number: 3');
         //console logging 
-        console.log(htmlElement.textContent);
+        console.log('Adding another incremented nummber: show print out 3: ' + htmlElement.textContent);
     });
 });
